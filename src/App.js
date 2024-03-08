b1e8b0317fe60ffb7438beea57a9de3c0033c2aa
zeenat <zeenu.23@gmail.com>
2024-03-08 13:28:45 +0530

    added Chakra UI theme and updated text styles for h1 and h2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { ChakraProvider } from '@chakra-ui/react'
 
 import HookForm from './components/HookForm'
+import theme from './theme'
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <HookForm/>
     </ChakraProvider>
   )
