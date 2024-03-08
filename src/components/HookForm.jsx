6c2319125b6276723fcf959b2ca3d881ceea7a34
zeenat <zeenu.23@gmail.com>
2024-03-08 19:28:26 +0530

    Added gender and date of birth fields to the form.

--- src/components/HookForm.jsx
+++ src/components/HookForm.jsx
@@ -5,11 +5,13 @@ import {
   Input,
   Flex,
   Button,
+  Select,
 } from '@chakra-ui/react'
 
 export default function HookForm() {
   return (
     <form>
+      <h1>Basic Details</h1>
       <Flex mb={4} gap={4}>
         <FormControl isInvalid={false} isRequired w="50%">
           <FormLabel htmlFor='firstname'>
@@ -37,14 +39,32 @@ export default function HookForm() {
       </FormControl>
 
       <FormControl isInvalid={false} isRequired>
-        <FormLabel htmlFor="phone"> Phone number</FormLabel>
+        <FormLabel htmlFor="phone">Phone number</FormLabel>
         <Input id="phone" placeholder="Enter Phone" name="phone" bg="#D9D9D9"/>
         <FormErrorMessage>
             Please enter a valid number.
         </FormErrorMessage>
-      </FormControl>
-        
+      </FormControl>       
       </Flex>
+
+      <h1>Other Information</h1>
+      <Flex mb={4} gap={4}>
+        <FormControl isInvalid={false} flexDir="column">
+          <FormLabel htmlFor="gender">Select Gender</FormLabel>
+          <Select>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            <option value="other">Other</option>
+          </Select>
+        </FormControl>
+
+        <FormControl id="dob" mb="4" isInvalid={false}>
+          <FormLabel htmlFor="dob">Date of Birth</FormLabel>
+          <Input placeholder="Select date" size="md" type="date" id="date-of-birth" name="dob" />
+        </FormControl>
+      </Flex>
+
+
       <Button type="submit">Submit</Button>
     </form>
   )
