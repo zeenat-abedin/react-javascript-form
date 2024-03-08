680a76421e230edbd83bc0cdd2ac9b5d4c1a48e1
zeenat <zeenu.23@gmail.com>
2024-03-08 14:17:58 +0530

    Add a title and wrap the form in a box.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { Box, ChakraProvider } from '@chakra-ui/react'
 
 import HookForm from './components/HookForm'
 import theme from './theme'
@@ -6,7 +6,13 @@ import theme from './theme'
 function App() {
   return (
     <ChakraProvider theme={theme}>
+      <Box textAlign='center' textStyle='h1'>
+        User Details
+      </Box>
+      <Box maxW="960px" p={4} mx="auto" bg="#F0EEEE">
       <HookForm/>
+      </Box>
+      
     </ChakraProvider>
   )
 }
--- src/components/HookForm.jsx
+++ src/components/HookForm.jsx
@@ -1,7 +1,36 @@
-import React from 'react'
+import {
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Input,
+  Flex,
+} from '@chakra-ui/react'
 
 export default function HookForm() {
   return (
-    <div>HookForm</div>
+    <form>
+      <Flex mb={4}>
+        <FormControl isInvalid={false} isRequired w="50%">
+          <FormLabel htmlFor='firstname'>
+            First name
+          </FormLabel>
+        </FormControl>
+        <FormControl isInvalid={false} isRequired w="50%">
+          <FormLabel htmlFor='lastname'>
+            Last name
+          </FormLabel>
+        </FormControl>
+      </Flex>
+      <Flex>
+      <FormControl isInvalid={false}>
+        <FormLabel htmlFor="email">Email address</FormLabel>
+        <Input id="email" name="email" />
+        <FormErrorMessage></FormErrorMessage>
+        <FormHelperText>We&apos;ll never share your email with anyone else.</FormHelperText>
+      </FormControl>
+      </Flex>
+
+    </form>
   )
 }
